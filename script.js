cde09b1aa6f83706e274cea9c5f9aaf45159fbaf
GuilhermeFuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2024-09-24 19:55:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,25 @@ function price(){
     let v = parseFloat(document.getElementById('v').value);
     let i = parseFloat(document.getElementById('i').value);
     let n = parseFloat(document.getElementById('n').value);
+    let juros = v * i;
     let p = (v * Math.pow(1+i , n) * i)/(Math.pow(1+i , n)-1);
+    let amor = p - juros;
+    let corpo = document.querySelector('#tabela tbody');
 
-    alert(p);
+    for (let par=1; par<=n; par++){
+        let linha =`
+            <tr>
+                <td>${par}</td>
+                <td>${p.toFixed(2)}</td>
+                <td>${juros.toFixed(2)}</td>
+                <td>${amor.toFixed(2)}</td>
+                <td>${v.toFixed(2)}</td>
+            </tr>
+            `;
+
+        corpo.innerHTML += linha;
+
+    }
+
+    
 }
