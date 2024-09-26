7d4f78a13dc9e5537de7fbedf0e49b60cf5b2361
GuilhermeFuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2024-09-26 11:40:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,21 +6,35 @@ function price(){
     let p = (v * Math.pow(1+i , n) * i)/(Math.pow(1+i , n)-1);
     let amor = p - juros;
     let corpo = document.querySelector('#tabela tbody');
+    let linha1 =`
+                <tr>
+                    <td>0</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>${v.toFixed(2)}</td>
+                </tr>
+                `;
 
-    for (let par=1; par<=n; par++){
-        let linha =`
-            <tr>
-                <td>${par}</td>
-                <td>${p.toFixed(2)}</td>
-                <td>${juros.toFixed(2)}</td>
-                <td>${amor.toFixed(2)}</td>
-                <td>${v.toFixed(2)}</td>
-            </tr>
-            `;
+    corpo.innerHTML = linha1;
 
-        corpo.innerHTML += linha;
+        for (let par=1; par<=n; par++){
+
+            let linha =`
+                <tr>
+                    <td>${par}</td>
+                    <td>${p.toFixed(2)}</td>
+                    <td>${juros.toFixed(2)}</td>
+                    <td>${amor.toFixed(2)}</td>
+                    <td>${(v-amor).toFixed(2)}</td>
+                </tr>
+                `;
+
+            corpo.innerHTML += linha;
+            v = v - amor;
+            juros = v * i;
+            amor = p - juros;
 
     }
 
-    
 }
